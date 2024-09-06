ee6984827cbad49857a11450efdeda4ff9d1ba07
p47n <patelmuhammed7777@gmail.com>
2024-09-06 12:30:24 -0230

    adding code for app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+from plscanner import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    result = None 
+    if request.method == "POST":
+        url = request.form["url"]
+        result = phishing_scanner(url)
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
