1f823482faf811f8ef297e0adff5c3cd4347d78b
p47n <patelmuhammed7777@gmail.com>
2024-09-06 15:51:29 -0230

    moved template to root fixed some bugs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from plscanner import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route("/", methods=["GET", "POST"])
 
